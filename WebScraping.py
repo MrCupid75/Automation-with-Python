b44aab4e5ddfbe590c695d7dd510352c000d177f
Beast <mjnuvor@gmail.com>
2025-05-12 21:05:37 +0000

    Refactor WebScraping.py to comment out example HTML content and related parsing code

--- WebScraping.py
+++ WebScraping.py
@@ -1,22 +1,23 @@
 from bs4 import BeautifulSoup
 
-html_content = """
-<html>
-<body>
-    <h1>This is a heading</h1>
-    <p>This is a paragraph.</p>
-    <p class="special">This is another paragraph.</p>
-</body>
-</html>
-"""
+# html_content = """
+# <html>
+# <body>
+#     <h1>This is a heading</h1>
+#     <p>This is a paragraph.</p>
+#     <p class="special">This is another paragraph.</p>
+# </body>
+# </html>
+# """
 
 soup = BeautifulSoup(html_content, 'html.parser')
 
-p_content = soup.find('p')
-# print(p_content)
+# p_content = soup.find('p')
+# # print(p_content)
 
-p_all_content = soup.find_all('p')
-# print(p_all_content)
+# p_all_content = soup.find_all('p')
+# # print(p_all_content)
 
-p_with_spec_class = soup.find('p', class_='special')
-print(p_with_spec_class)
+# p_with_spec_class = soup.find('p', class_='special')
+# print(p_with_spec_class)
+
