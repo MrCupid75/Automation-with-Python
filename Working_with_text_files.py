e5f114c4020d03fc56cb914d01c2ccc171282c5b
Beast <mjnuvor@gmail.com>
2025-05-12 11:19:06 +0000

    CSV writing and reading in Working_with_text_files.py; create output.csv with sample data

--- Working_with_text_files.py
+++ Working_with_text_files.py
@@ -11,9 +11,28 @@
 # print(text)
 
 #Appending to text files
-with open('newFile.txt' , 'a') as f:
-    f.write("\nFeels to be confident in your skill and to be selfish on your goals")
+# with open('newFile.txt' , 'a') as f:
+#     f.write("\nFeels to be confident in your skill and to be selfish on your goals")
 
-with open('newFile.txt', 'r') as f:
-    newContent = f.read()
-    print(newContent)
+# with open('newFile.txt', 'r') as f:
+#     newContent = f.read()
+#     print(newContent)
+
+#writing in csv file
+
+import csv
+
+# data = [
+#     ['Name', 'Age', 'City'],
+#     ['Alice', '24', 'New York'],
+#     ['Bob', '27', 'Los Angeles']   
+# ]
+
+# with open('output.csv', 'w', newline='') as f:
+#     writer = csv.writer(f)
+#     writer.writerows(data)
+
+with open('output.csv', 'r') as f:
+    read_csv = csv.reader(f)
+    for row in read_csv:
+        print(row)
