7a5a85d8e67694b7a1165bfd39dc85cd43dc9781
Beast <mjnuvor@gmail.com>
2025-05-13 10:19:55 +0000

    Add car listing extraction logic to BeautifulSoapPersonalTask.py

--- BeautifulSoapPersonalTask.py
+++ BeautifulSoapPersonalTask.py
@@ -9,3 +9,16 @@ except Exception as e:
     print(f"Error fetching webpage {e}")
 else:
     soup = BeautifulSoup(response.content, 'html.parser')
+
+main_container = soup.find('div', class_='container')
+
+car_cards = main_container.find_all('div', class_='js-advert-list-item')
+
+print(car_cards)
+
+for card in car_cards:
+    car_name = card.find('div', class_='qa-advert-title')
+
+    price = card.find('div', class_='qa-advert-price')
+
+    print(car_name)
